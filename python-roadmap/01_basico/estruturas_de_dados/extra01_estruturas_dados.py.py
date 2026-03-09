4b70f33821848bc77de3e4e77528fb9649d947c7
Tais Prates <taisbezerra24777@gmail.com>
2026-03-09 19:02:16 -0300

    estrutura de dados: exercícios extras

--- python-roadmap/01_basico/estruturas_de_dados/extra01_estruturas_dados.py.py
+++ python-roadmap/01_basico/estruturas_de_dados/extra01_estruturas_dados.py.py
@@ -5,3 +5,25 @@
 ##1. Mostre todos os números digitados.
 ##2. Mostre o maior número.
 ##3. Mostre o menor número.
+
+numeros = []
+
+for i in range(5):
+    numero = input('Digite um número inteiro: ')
+    numeros.append(numero)
+print(numeros)
+
+menor = numeros[0]
+
+for numero in numeros:
+    if numero < menor:
+        menor = numero
+print(menor)
+
+
+maior = numeros[0]
+
+for numero in numeros:
+    if numero > maior:
+        maior = numero
+print(maior)
--- python-roadmap/01_basico/estruturas_de_dados/extra02_estruturas_dados.py
+++ python-roadmap/01_basico/estruturas_de_dados/extra02_estruturas_dados.py
@@ -4,3 +4,12 @@
 #Peça ao usuário 5 nomes e armazene em uma lista. Depois:
 ##1. Mostre os nomes um por linha.
 ##2. Mostre os nomes numerados.
+
+nomes = []
+
+for i in range(5):
+    nomes = input('Digite um nome: ')
+    numeros.append(numero)
+print(numeros)
+
+menor = numeros[0]
